067842b7a55e0c00e9466fb6cccbc55adde10dde
Lasse Broust Andersen <Broust@hotmail.com>
2016-05-18 10:14:23 +0200

    Connect class

---
+++ smil/smil/Connect.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+
+namespace smil
+{
+    class Connect
+    {
+        private static Connect Connection;
+        public SqlConnection Con;
+        // Constructor is 'protected'
+        protected Connect()
+        {
+            SqlConnection Con = new SqlConnection("user id=root;" +
+                                       "password=kony2012;" + 
+                                       "server=holydb;" +
+                                       "Trusted_Connection=yes;" +
+                                       "database=SMIL; " +
+                                       "connection timeout=30");
+        }
+
+        public static Connect Conn()
+        { 
+            if (Connection == null)
+            {
+                Connection = new Connect();
+            }
+
+            return Connection;
+        }
+    }
+}
