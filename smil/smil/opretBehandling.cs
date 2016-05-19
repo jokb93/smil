831f2322eaeeffdbc3f2c39ea5ea74dd4f70c2d6
joachim bertelsen <bertleren@gmail.com>
2016-05-19 10:54:12 +0200

    opret behandling færdig

---
+++ smil/smil/opretBehandling.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace smil
+{
+    class opretBehandling
+    {
+
+        public returnObj opretTid(int type, int patientid, DateTime date, int lokaleId, int personaleId)
+        {
+            if (Connect.query("INSERT INTO `SMIL`.`behandling` (`id`, `type`, `patientid`, `dato`) VALUES (NULL, '" + type + "', '" + patientid + "', '" + date.ToString("yyyy-MM-dd HH:mm:ss") + "');"))
+            {
+                int behandlingsId = Connect.insertID();
+                if (Connect.query("INSERT INTO `SMIL`.`behandlingslokale` (`behandlingid`, `lokaleid`) VALUES ('"+ behandlingsId + "', '"+ lokaleId+ "');"))
+                {
+                    if (Connect.query("INSERT INTO `SMIL`.`behandlingspersonale` (`behandlingsid`, `personaleid`) VALUES ('" + behandlingsId + "', '" + personaleId + "');"))
+                    {
+                        returnObj Arr = new returnObj(3);  // type 3 er opret behandling
+                        Arr.Add("Behandling oprettet");
+
+                        return Arr;
+                    }
+                    else
+                    {
+                        returnObj Arr = new returnObj(0);  // type 2 er opret lokale
+                        Arr.Add("forbindelses fejl");
+
+                        return Arr;
+                    }
+                } else
+                {
+                    returnObj Arr = new returnObj(0);  // type 2 er opret lokale
+                    Arr.Add("forbindelses fejl");
+
+                    return Arr;
+                }
+                    
+
+            }
+            else
+            {
+                returnObj Arr = new returnObj(0);  // type 2 er opret lokale
+                Arr.Add("forbindelses fejl");
+
+                return Arr;
+            }
+        }
+        
+    }
+}
