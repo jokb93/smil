99fcc36ab9a395c6de75e60d8c6cf77ef286fdbb
Emilie <emiliebang@outlook.dk>
2016-05-20 10:47:44 +0200

    UI update 1

--- smil/smil/Form1.Designer.cs
+++ smil/smil/Form1.Designer.cs
@@ -28,33 +28,289 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.statusbar = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.personale = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lokale = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tid = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.rediger = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.slet = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.arbejder = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // statusbar
+            // 
+            this.statusbar.AutoSize = true;
+            this.statusbar.Location = new System.Drawing.Point(0, 460);
+            this.statusbar.Name = "statusbar";
+            this.statusbar.Size = new System.Drawing.Size(58, 13);
+            this.statusbar.TabIndex = 0;
+            this.statusbar.Text = "status: klar";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.personale,
+            this.id,
+            this.lokale,
+            this.tid,
+            this.rediger,
+            this.slet});
+            this.dataGridView1.Location = new System.Drawing.Point(318, 0);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(393, 466);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(105, 31);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 2;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Vælg dato:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Vælg personale:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(128, 6);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Filtrer tider";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "mand",
+            "mand"});
+            this.comboBox1.Location = new System.Drawing.Point(105, 63);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(200, 21);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "mand",
+            "mand"});
+            this.comboBox2.Location = new System.Drawing.Point(105, 95);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(200, 21);
+            this.comboBox2.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 99);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Vælg lokale:";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(180, 139);
+            this.button1.Location = new System.Drawing.Point(12, 427);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Size = new System.Drawing.Size(293, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Opret ny tid";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 398);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(293, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Opret/Rediger kunder";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 369);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(293, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Opret/Rediger Personale";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // personale
+            // 
+            this.personale.HeaderText = "Personale";
+            this.personale.Name = "personale";
+            this.personale.ReadOnly = true;
+            // 
+            // id
+            // 
+            this.id.HeaderText = "id";
+            this.id.Name = "id";
+            this.id.ReadOnly = true;
+            this.id.Visible = false;
+            // 
+            // lokale
+            // 
+            this.lokale.HeaderText = "Lokale";
+            this.lokale.Name = "lokale";
+            this.lokale.ReadOnly = true;
+            // 
+            // tid
+            // 
+            this.tid.HeaderText = "Tid";
+            this.tid.Name = "tid";
+            this.tid.ReadOnly = true;
+            // 
+            // rediger
+            // 
+            this.rediger.DataPropertyName = "Rediger";
+            this.rediger.HeaderText = "Rediger";
+            this.rediger.Name = "rediger";
+            this.rediger.ReadOnly = true;
+            this.rediger.Text = "Rediger";
+            this.rediger.ToolTipText = "Rediger";
+            this.rediger.UseColumnTextForButtonValue = true;
+            this.rediger.Width = 60;
+            // 
+            // slet
+            // 
+            this.slet.DataPropertyName = "Slet";
+            this.slet.HeaderText = "Slet";
+            this.slet.Name = "slet";
+            this.slet.ReadOnly = true;
+            this.slet.Text = "Slet";
+            this.slet.ToolTipText = "Slet";
+            this.slet.UseColumnTextForButtonValue = true;
+            this.slet.Width = 30;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "mand",
+            "mand"});
+            this.comboBox3.Location = new System.Drawing.Point(105, 128);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(200, 21);
+            this.comboBox3.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 132);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Vælg patient:";
+            // 
+            // arbejder
+            // 
+            this.arbejder.Enabled = false;
+            this.arbejder.Location = new System.Drawing.Point(464, 369);
+            this.arbejder.Name = "arbejder";
+            this.arbejder.Size = new System.Drawing.Size(247, 109);
+            this.arbejder.TabIndex = 14;
+            this.arbejder.Text = "Arbejder! vent venligst...";
+            this.arbejder.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(487, 369);
+            this.ClientSize = new System.Drawing.Size(711, 475);
+            this.Controls.Add(this.arbejder);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.statusbar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Oversigt";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
+        private System.Windows.Forms.Label statusbar;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn personale;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn lokale;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tid;
+        private System.Windows.Forms.DataGridViewButtonColumn rediger;
+        private System.Windows.Forms.DataGridViewButtonColumn slet;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button arbejder;
     }
 }
--- smil/smil/Form1.cs
+++ smil/smil/Form1.cs
@@ -10,11 +10,14 @@ using System.Windows.Forms;
 
 namespace smil
 {
+
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+            arbejder.Visible = false;
+            arbejder.Dock = DockStyle.Fill;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -22,6 +25,49 @@ namespace smil
             Personale person = new Personale();
             returnObj res = person.retPersonale("Martin Hanse", "Elev", 2);
             MessageBox.Show(res[1]);
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            arbejder.Visible = true;
+            PersonaleForm form = new PersonaleForm();
+            form.FormClosed += new FormClosedEventHandler(ChildFormClosed);
+            form.Show();
+        }
+
+        void ChildFormClosed(object sender, FormClosedEventArgs e)
+        {
+            arbejder.Visible = false;
         }
     }
 }
